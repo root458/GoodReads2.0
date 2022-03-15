8cf2625ce8e176c3dd3512bbd5cf9345d4e5d176
Hope Kagame <kagame458@protonmail.com>
2022-03-15 21:44:01 +0300

    Add bloc observer Set up main_development

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,23 +5,48 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:good_reads_app/app/app.dart';
-import 'package:good_reads_app/bootstrap.dart';
+import 'package:good_reads_app/services/_index.dart';
+import 'package:good_reads_app/training/training_bloc_observer.dart';
 import 'package:good_reads_app/utils/_index.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   TrainingConfig(
     values: TrainingValues(
       baseDomain: 'https://jsonkeeper.com',
       authBox: 'training_development',
     ),
   );
-  
-  bootstrap(
-    () => MultiBlocProvider(
-      providers: Singletons.registerCubits(),
-      child: const App(),
-    ),
+
+  await HiveServiceImpl().initBoxes();
+  await Firebase.initializeApp();
+
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  await runZonedGuarded(
+    () async {
+      await BlocOverrides.runZoned(
+        () async => runApp(
+          MultiBlocProvider(
+            providers: Singletons.registerCubits(),
+            child: const App(),
+          ),
+        ),
+        blocObserver: TrainingBlocObserver(),
+      );
+    },
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
